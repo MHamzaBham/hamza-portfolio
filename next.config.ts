5c985fbb9c82293d0077328ddd66e01256b0c6be
Hamza Bham <92208535+MHamzaBham@users.noreply.github.com>
2025-04-05 19:45:06 +0500

    assets configuration for production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Required for static export
+  basePath: process.env.NODE_ENV === 'production' ? '/hamza-portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/hamza-portfolio/' : '',
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
 };
 
 export default nextConfig;
--- src/app/ui/Logo.tsx
+++ src/app/ui/Logo.tsx
@@ -3,7 +3,7 @@
 export default function Logo() {
   return (
     <a href="#" className="flex items-center mb-8">
-      <img src="/assets/hamza_prof.jpg" className="h-10 border-2 border-gray-500 rounded-full" alt="Logo" />
+      <img src={`${process.env.NODE_ENV === 'production' ? '/hamza-portfolio' : ''}/assets/hamza_prof.jpg`} className="h-10 border-2 border-gray-500 rounded-full" alt="Logo" />
     </a>
   );
 }
